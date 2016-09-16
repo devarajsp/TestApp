836fbba64288d797999de9e8b1f7040094bf1215
devarajsp <devarajsp@gmail.com>
2016-09-16 22:50:11 +0530

    Add files via upload

---
+++ data/localapps.js
@@ -0,0 +1,8 @@
+var localappslist = '[' + 
+	'{"menuName":"Blood Bank", "menuHref":"http://google.com", "colorStyleText": "color-7", "menuSize":"small", "faIconText":"fa-bank", "styleText":"null", "menuColumn":"1", "roleTags":["ym","yf","children","pregnant"], "locationTags":["all"] , "ailmentTags":["diabetes", "cancer"]   },' +
+   	'{"menuName":"Call Nurses", "menuHref":"http://google.com", "colorStyleText": "color-8", "menuSize":"small", "faIconText":"fa-medkit", "styleText":"null", "menuColumn":"1", "roleTags":["am","af","sm","sf","pregnant"], "locationTags":["all"] , "ailmentTags":["diabetes", "cancer"]   },' +
+	'{"menuName":"Hospital nearby", "menuHref":"http://google.com", "colorStyleText": "color-9", "menuSize":"big", "faIconText":"fa-child", "styleText":"null", "menuColumn":"2", "roleTags":["sm","sf","children","pregnant"], "locationTags":["all"] , "ailmentTags":["diabetes", "cancer"]   },' +
+	'{"menuName":"First Mom Club", "menuHref":"http://google.com", "colorStyleText": "color-10", "menuSize":"big", "faIconText":"fa-child", "styleText":"null", "menuColumn":"2", "roleTags":["yf","pregnant"], "locationTags":["all"] , "ailmentTags":["diabetes", "cancer"]   },' +
+	'{"menuName":"Gynic FAQ", "menuHref":"http://google.com", "colorStyleText": "color-7", "menuSize":"big", "faIconText":"fa-child", "styleText":"null", "menuColumn":"2", "roleTags":["yf","af", "sf"], "locationTags":["all"] , "ailmentTags":["diabetes", "cancer"]   },' +
+	'{"menuName":"Health History", "menuHref":"http://google.com", "colorStyleText": "color-8", "menuSize":"small", "faIconText":"fa-hospital", "styleText":"null", "menuColumn":"3", "roleTags":["male","female","children","pregnant"], "locationTags":["all"] , "ailmentTags":["diabetes", "cancer"]   }' +
+         ']';
